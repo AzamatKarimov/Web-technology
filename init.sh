d156f4f83e9fede2e190a76384f285eda3d8efb1
AzamatKarimov <62419683+AzamatKarimov@users.noreply.github.com>
2020-04-26 20:51:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,16 @@
+# скрипт инициализации 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/test_django
+
+sudo service nginx restart
+#sudo service gunicorn stop
+
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello.app 
+#sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf wsgi:application 
+#sudo service gunicorn restart
+#sudo service mysql start
+
+sudo gunicorn -b 0.0.0.0:8080 hello:app&
+cd /home/box/web/ask
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application&
