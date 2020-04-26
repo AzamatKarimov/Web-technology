24851dda3e3888ccb27bdc305c37a00a1c0c2fd9
AzamatKarimov <62419683+AzamatKarimov@users.noreply.github.com>
2020-04-26 20:50:31 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 def app(environ, start_response):
-    body = '[bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]'
-	start_response('200 OK', [('Content-Type', 'text/plain')])
-	return body
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [(item + '\n').encode() for item in environ['QUERY_STRING'].split('&')]
     
     
     #gunicorn --bind='0.0.0.0:8080' hello:test
